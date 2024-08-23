9d4e740b051d5e41794a945680d255d3dc40515e
HiepVo <hoclaptrinh1012005@gmail.com>
2024-08-23 15:26:05 +0700

    Change redis Url

--- src/db/redis.db.ts
+++ src/db/redis.db.ts
@@ -3,19 +3,21 @@ import { logger } from "../logs";
 
 const portEnv = process.env.REDIS_PORT;
 
-const configRedis: RedisOptions = {
-    host: process.env.REDIS_HOSTNAME,
-    port: portEnv ? Number.parseInt(portEnv) : 6379,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    retryStrategy: function (count: number) {
-        logger.debug(`Reconnecting Attempt Number: ${count}`);
-        const delay = Math.min(count * 100, 3000);
-        return delay;
-    },
-};
+// const configRedis: RedisOptions = {
+//     host: process.env.REDIS_HOSTNAME,
+//     port: portEnv ? Number.parseInt(portEnv) : 6379,
+//     // username: process.env.REDIS_USERNAME,
+//     // password: process.env.REDIS_PASSWORD,
+//     retryStrategy: function (count: number) {
+//         logger.debug(`Reconnecting Attempt Number: ${count}`);
+//         const delay = Math.min(count * 100, 3000);
+//         return delay;
+//     },
+// };
 
-export const redisClient = new Redis(configRedis);
+export const redisClient = new Redis(
+    process.env.REDIS_URI || "http://localhost"
+);
 
 const statusConnectRedis = {
     CONNECT: "connect",
@@ -26,16 +28,22 @@ const statusConnectRedis = {
 
 const handleEventConnection = () => {
     redisClient.on(statusConnectRedis.CONNECT, () => {
-        logger.info("connectionRedis - Connection status: connected successfully");
+        logger.info(
+            "connectionRedis - Connection status: connected successfully"
+        );
     });
     redisClient.on(statusConnectRedis.END, () => {
         logger.info("connectionRedis - Connection status: disconnected");
     });
     redisClient.on(statusConnectRedis.RECONNECT, () => {
-        logger.info("connectionRedis - Connection status: Redis is reconnecting");
+        logger.info(
+            "connectionRedis - Connection status: Redis is reconnecting"
+        );
     });
     redisClient.on(statusConnectRedis.ERROR, (err) => {
-        logger.error(`Error connecting to Redis, Please try again later. ${err}`);
+        logger.error(
+            `Error connecting to Redis, Please try again later. ${err}`
+        );
     });
 };
 
